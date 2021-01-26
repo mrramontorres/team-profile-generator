59aeefa9756e981ce785ba05862d4e25d4d6e97f
turtles <me@ramon.nyc>
2021-01-26 12:11:42 -0800

    moved npm files to folder

---
+++ index.js
@@ -0,0 +1,32 @@
+// fs is a Node standard library package for reading and writing files
+const fs = require('fs'); 
+// inquire is a promise-based Node package used to create CLI (Command Line Interface) tools for query-based tasks.
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
