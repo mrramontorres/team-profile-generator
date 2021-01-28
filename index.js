cee5141c53146c947af15be398a3cfab65b68753
turtles <me@ramon.nyc>
2021-01-27 17:27:01 -0800

    added functions back to employee.js file.

--- index.js
+++ index.js
@@ -3,40 +3,48 @@ const inquirer = require('inquirer');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require('./lib/Employee');
 
 
 function appMenu() {
-    const employee = inquirer
-    .prompt([
+
+    const employee = inquirer.prompt([
     {
-        type: "input",
-        name: "name",
-        message: "What's your name?",
+    type: "input",
+    name: "name",
+    message: "What's your name?",
     },
     {
-        type: "input",
-        name: "id",
-        message: "What's your employee id?",
+    type: "input",
+    name: "id",
+    message: "What's your employee id?",
     },
     {
-        type: "input",
-        name: "email",
-        message: "What's your email?",
+    type: "input",
+    name: "email",
+    message: "What's your email?",
     },
     {
-        type: "confirm",
-        name: "choice",
-        message: "Would you like to add more employees?"
-      }
+    type: "confirm",
+    name: "choice",
+    message: "Would you like to add more employees?"
+    }
     ])
+
+    .then((data) => {
+        console.log(data);
+    })
+    
+    /*
     .then((val) => {
         if (val.choice) {
-            this.appMenu();
+            console.log("Need to add more....");
         } else {
-            console.log("Done.")
+            console.log("Done.");
         }
-        console.log(data)
-    });
+    })
+    */
+
 }
 
 appMenu();
