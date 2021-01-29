1d37d5c504cf6369370801c998e4b47af11df6d4
turtles <me@ramon.nyc>
2021-01-29 00:05:42 -0800

    successfuly another person to the html.

--- index.js
+++ index.js
@@ -155,31 +155,31 @@ function pageStart() {
     })
 };
 function engineerHTML(newEmployee) {
-    console.log(newEmployee);
-    console.log(newEmployee.getName());
+  return new Promise(function(resolve, reject) {
+   const newName = newEmployee.getName();
    const data =
     `
         <div class="card employee-card">
         <div class="card-header">
-            <h2 class="card-title">${newEmployee.getName()}</h2>
+            <h2 class="card-title">${newName}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${newEmployee.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
                 <li class="list-group-item">ID: ${newEmployee.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${newEmployee.getEmail()}}">${newEmployee.getEmail()}</a></li>
-                //<li class="list-group-item">GitHub: <a href="https://github.com/${newEmployee.getGithub()}" target="_blank" rel="noopener noreferrer">${newEmployee.getGithub()}</a></li>
+
             </ul>
         </div>
     </div>
-    `;
+    `; 
     fs.appendFile("./dist/sampleTeam.html", data, function(err) {
         if (err) {
-            console.log(err); ``
+            return reject(err);
         }
+        return resolve();
     })
-
-};
+})}; 
 function managerHTML(employeeManager) {
     const data =
     `
